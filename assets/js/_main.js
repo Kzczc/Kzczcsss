bb8cddbb02a5aeefc002bb502d2a3a4e08e2a265
Kc <>
2025-12-08 00:30:04 +0800

    Beautify UI: new fonts, frosted navbar, polished sidebar and smooth scroll fix

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -58,7 +58,39 @@ $(document).ready(function(){
   });
 
   // init smooth scroll
-  $("a").smoothScroll({offset: -20});
+  // $("a").smoothScroll({offset: -20});
+
+  // Custom smooth scroll implementation for navigation
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+    // Check if the link is for the current page
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 60 // Offset for sticky header
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
