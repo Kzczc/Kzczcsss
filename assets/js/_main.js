c9b7032d08b49e0630a238752bac2dbf6bc236ec
Kc <>
2025-12-08 00:39:09 +0800

    Enhance navbar: rainbow gradient hover, dynamic underline, and spacing fix

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
   // $("a").smoothScroll({offset: -20});
 
   // Custom smooth scroll implementation for navigation
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+  $('.greedy-nav a[href*="#"]').click(function(event) {
     // Check if the link is for the current page
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && 
@@ -75,19 +75,8 @@ $(document).ready(function(){
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top - 60 // Offset for sticky header
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+          scrollTop: target.offset().top - 80 // Increased Offset for sticky header
+        }, 800); // Slightly faster animation
       }
     }
   });
